438b144aab61770f4ef753d99f8a9d47de4f65c0
esteban <stephendavidboyd@gmail.com>
2015-02-09 22:40:30 -0500

    beginnings

--- constellation.js
+++ constellation.js
@@ -1,18 +1,9 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.body.helpers({
+    posts: [
+      { text: "This is a seed post" },
+      { text: "This is another seed post"}
+    ]
   });
 }
 
