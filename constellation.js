e4c8d33f51deefc37fbec402115c3b1bc314b045
esteban <stephendavidboyd@gmail.com>
2015-02-10 22:43:13 -0500

    a simple posting interface with insecure user accounts

--- constellation.js
+++ constellation.js
@@ -1,9 +1,38 @@
+Posts = new Mongo.Collection("posts");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    posts: [
-      { text: "This is a seed post" },
-      { text: "This is another seed post"}
-    ]
+    posts: function () {
+      return Posts.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-post": function (event) {
+      var text = event.target.text.value;
+      Posts.insert({
+        text: text,
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
+      });
+      event.target.text.value = "";
+      return false;
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+  Template.post.events({
+    "click .delete": function () {
+      Posts.remove(this._id);
+    }
+  });
+
+  Handlebars.registerHelper("prettifyDate", function(timestamp) {
+    return moment(new Date(timestamp)).fromNow();
   });
 }
 
