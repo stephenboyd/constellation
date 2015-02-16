ee5767aa2dacb7512287a6471dcaa1c05337af61
esteban <stephendavidboyd@gmail.com>
2015-02-15 20:16:11 -0500

    added strict schema validation for posts

---
+++ common.js
@@ -0,0 +1,38 @@
+Posts = new Mongo.Collection("posts");
+Posts.attachSchema(new SimpleSchema({
+  title: {
+    type: String,
+    label: "Title",
+    max: 60,
+    optional: true
+  },
+  createdAt: {
+    type: Date,
+    label: "Date"
+  },
+  owner: {
+    type: String,
+    label: "Owner"
+  },
+  username: {
+    type: String,
+    label: "Username"
+  },
+  image: {
+    type: String,
+    label: "Image",
+    optional: true
+  },
+  text: {
+    type: String,
+    label: "text"
+  }
+}));
+
+
+
+if (Meteor.isServer) {
+  Meteor.publish("posts", function () {
+    return Posts.find();
+  });
+}
--- constellation.js
+++
@@ -1,7 +0,0 @@
-Posts = new Mongo.Collection("posts");
-
-if (Meteor.isServer) {
-  Meteor.publish("posts", function () {
-    return Posts.find();
-  });
-}
